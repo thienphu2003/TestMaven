26299ca502253ab14322db1cd15c457b96a06f1c
Keyuky <nguyenminhvu031202@gmail.com>
2023-03-16 16:51:57 +0700

    ticket2 vu choi phu

--- test-maven-with-git/src/main/java/com/example/testmavenwithgit/HelloServlet.java
+++ test-maven-with-git/src/main/java/com/example/testmavenwithgit/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Vũ Nữ";
+        message = "phú bê đê";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
